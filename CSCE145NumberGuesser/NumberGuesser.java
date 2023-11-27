ec6e6491bf6062797942bfc67162995415106a57
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-11-27 10:53:34 -0500

    Used While Loop to make simple Number Guesser game

---
+++ CSCE145NumberGuesser/NumberGuesser.java
@@ -0,0 +1,37 @@
+/*
+ * Written by Kirk Allgeyer
+ */
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGuesser {
+
+    public static final int UPPER_NUMBER = 100;
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        Random rand = new Random();
+        int secretNumber = rand.nextInt(UPPER_NUMBER);
+
+        System.out.println("I'm thinking of a number from 0 to "+(UPPER_NUMBER-1) +"\nGuess the number!");
+
+        int guessNumber = 0;
+        boolean correctGuess = false;
+
+         while(!correctGuess) {
+
+            guessNumber = keyboard.nextInt();
+            if(guessNumber > secretNumber) {
+                System.out.println("That's too high!");
+            }
+            else if(guessNumber < secretNumber) {
+                System.out.println("That's too low!");
+            }
+            else {
+                System.out.println("That's correct");
+                correctGuess = true; 
+            }
+         }
+
+    }
+
+}
