5e37c4749c1bb8bad7eb17d14495c6ec8182aa9d
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-11-30 11:13:45 -0500

    Hide and Seek Game Complete
    Completed "Hide and Seek" game project.
    
    Learned about using Nested For Loops with Multi-Dimensional arrays, using constants to create values that are consistent throughout the program's lifecycle, using some simple square value math to calculate relative distance, and using multiple conditionals to validate player choices, the game state, and "refresh" the game state through a while loop.

--- CSCE145HideAndSeek/HideAndSeek.java
+++ CSCE145HideAndSeek/HideAndSeek.java
@@ -65,6 +65,8 @@ public class HideAndSeek {
 
         //While the game isn't over, repeat this gameplay loop
         while(!gameOver) {
+            //Print out the Board initially and each time the player completes
+            // a "move".
             for(int i=0; i<board.length; i++) {
                 for(int j=0; j<board[i].length; j++) {
                     System.out.print(board[i][j]);
@@ -72,8 +74,21 @@ public class HideAndSeek {
                 System.out.println();
             }
 
+            /*
+             * Manually calculate the squared delta values
+             * to represent the distance between the player
+             * position and the goal position
+             */
             int distance = (pX-gX)*(pX-gX)+(pY-gY)*(pY-gY);
 
+            /*
+             * If the calculated distance each loop is 
+             * greater than the Cold or Warm distance
+             * the respective notification will print.
+             * If it's less than those values that means it's
+             * equal to or less than them and therefore closer
+             * to the goal.
+             */
             if(distance > COLD_DIST) {
                 System.out.println("You are getting colder");
             }
@@ -84,6 +99,12 @@ public class HideAndSeek {
                 System.out.println("You're getting hotter");
             }
 
+            /*
+             * Control prompts to let the user move 1 index
+             * positively or negatively in x, y, or both at the same time
+             * Player can't move more than 1 index at a time and out
+             * of range values will be set to 0 instead.
+             */
             System.out.println("Enter -1, 0, or 1 to move in the X direction");
             int dX = keyboard.nextInt();
             System.out.println("Enter -1, 0, or 1 to move in the Y direction");
@@ -98,7 +119,62 @@ public class HideAndSeek {
                 dY = 0;
             }
 
+            /*
+             * Assign the space the player was last located
+             * to have the WALKED_PATH constant character.
+             */
+            board[pY][pX] = WALKED_PATH;
+
+            /*
+             * After assigning previous space the WALKED_PATH
+             * character, then increment the player's position
+             * on the board
+             */
+            pX += dX;
+            pY += dY;
+
+            /*
+             * If the player would move "out of bounds"
+             * of the board/arrays, prevent them from doing so by
+             * setting their position to either 0
+             * or the current BOARD_SIZE-1
+             */
+            if(pX < 0) {
+                pX = 0;
+            }
+            else if(pX > BOARD_SIZE-1) {
+                pX = BOARD_SIZE-1;
+            }
+
+
+            if(pY < 0) {
+                pY = 0;
+            }
+            else if(pY > BOARD_SIZE-1) {
+                pY = BOARD_SIZE-1;
+            }
+
+            /*
+             * After error-checking/validation, then
+             * "move" the player to the appropriate indicies
+             * on the board
+             */
+            board[pY][pX] = PLAYER;
+
+            /*
+             * When the player's position is equal to
+             * the goal's X and Y position in the board/arrays
+             * The player wins the game and the loop concludes.
+             */
+            if(pX == gX && pY == gY) {
+                System.out.println("You Win!");
+                gameOver = true;
+            }
         }
+        /*
+         * Stream closing recommended practice.
+         * May not be recommended/necessary for System.in
+         */
         keyboard.close();
     }
 
