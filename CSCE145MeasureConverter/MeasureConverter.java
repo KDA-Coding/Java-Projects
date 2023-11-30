7d13498977a9a0409200427dc5be2c29ba8e9011
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-11-30 15:29:10 -0500

    Beginning MeasureConverter.java
    Using Methods within a class to containerize functionality of a Measurement Conversion app.

---
+++ CSCE145MeasureConverter/MeasureConverter.java
@@ -0,0 +1,26 @@
+/*
+ * Written by Kirk Allgeyer
+ */
+import java.util.Scanner;
+public class MeasureConverter {
+
+    public static void main(String[] args) {
+        
+        MeasureConverter m = new MeasureConverter();
+        m.start();
+    }
+
+    public void start() {
+        Scanner keyboard = new Scanner(System.in);
+        printGreetings();
+        boolean quit = false;
+        while(!quit) {
+            
+        }
+    }
+
+    public void printGreetings() {
+        System.out.println("Welcome to the Units Converter!");
+    }
+
+}
