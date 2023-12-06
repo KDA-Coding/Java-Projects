b5d8ec86e0253db2d5ad4ef0b7567a6ec5a56833
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-12-06 16:03:58 -0500

    Created Cat Class Constructors, Getters, Setters, .equals, .toString, and Class specific Methods

--- CSCE145CatProject/Cat.java
+++ CSCE145CatProject/Cat.java
@@ -4,6 +4,98 @@
 
  public class Cat {
 
-    
+    private String name;
+    private double weight;
+    private int numberOfLegs;
 
- }
+    public Cat() {
+        this.name = "None";
+        this.weight = 1.0;
+        this.numberOfLegs = 4;
+    }
+
+    public Cat(String aN, double aW, int aL) {
+
+        this.setName(aN);
+        this.setWeight(aW);
+        this.setNumberOfLegs(aL);
+
+    }
+
+    //Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public int getNumberOfLegs() {
+        return this.numberOfLegs;
+    }
+
+
+    //Setters
+    public void setName(String aN) {
+
+        if(aN != null) {
+            this.name = aN;
+        }
+        else {
+            this.name="none";
+        }
+    }
+
+    public void setWeight(double aW) {
+
+        if(aW > 0.0) {
+            this.weight = aW;
+        }
+        else {
+            this.weight=1.0;
+        }
+    }
+
+    public void setNumberOfLegs(int aL) {
+
+        if(aL >= 0.0 && aL <= 4) {
+            this.numberOfLegs = aL;
+        }
+        else {
+            this.numberOfLegs = 4;
+        }
+
+    }
+
+    //Same Instance Boolean Check
+    public boolean equals(Cat aC) {
+        
+        return aC != null &&
+            this.name.equals(aC.getName()) &&
+            this.weight == aC.getWeight() &&
+            this.numberOfLegs == aC.getNumberOfLegs();
+    }
+
+    //ToString for Printing any Cat Instance's properties
+    //Useful for debugging
+    public String toString() {
+        return this.name + " " + 
+            this.weight + " " +
+            this.numberOfLegs;
+    }
+ 
+    // "Action" Methods
+
+    public void eat() {
+        this.weight += 0.2;
+    }
+
+    public void sleep() {
+        System.out.println("Zzz. Cat snore.");
+    } 
+
+    public void destroyYourStuff() {
+        System.out.println("I am become "+this.name+" Destroyer of Worlds!");
+    }
+}
--- CSCE145CatProject/HouseOfCats.java
+++ CSCE145CatProject/HouseOfCats.java
@@ -6,6 +6,28 @@ public class HouseOfCats {
     public static void main(String[] args) {
         
         Cat cat01;
+        Cat cat02;
+
+        cat01 = new Cat();
+        cat02 = new Cat("Triscuit", 9.2, 4);
+
+        System.out.println(cat01.getName()+" "+cat01.getWeight()+" "+cat01.getNumberOfLegs());
+
+        System.out.println(cat02.getName()+" "+cat02.getWeight()+" "+cat02.getNumberOfLegs());
+
+        cat01.setName("Mr.Flufferkins");
+        cat01.setWeight(8.0);
+
+        System.out.println(cat01.getName()+" "+cat01.getWeight()+" "+cat01.getNumberOfLegs());
+
+        Cat cat03 = new Cat("Mr.Flufferkins", 8.0, 4);
+
+        //Is False
+        System.out.println(cat01.equals(cat02));
+        //Is True
+        System.out.println(cat01.equals(cat03));
+        //Is False; they don't share the same memory address
+        System.out.println(cat01 == cat03);
 
     }
 
